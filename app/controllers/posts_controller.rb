6e917652bb25bc6882ac967197d8cfdac75ef643
Liane Allen <liantics.antics@gmail.com>
2014-07-21 13:23:45 -0400

    Add index to routes, remove create from controller
    Since this commit is supposed to be limited to displaying a posts index
    page, removed the create boilerplate from the controller to keep the
    commit cleaner.
    
    Added a posts index only resource to routes.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,4 @@
 class PostsController < ApplicationController
-  def create
-    
-  end
 
   def index
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :posts, only: [:index]
+
 
   root to: "posts#index"
 end
