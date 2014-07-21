1ade8cc463cf1b0bf1e59106bdd7dd1a43e06c57
Chris Thorsen <chris@chris.localdomain>
2014-07-21 15:47:47 -0400

    add unique index to locations horizontal spaceing

--- db/migrate/20140721152826_create_locations.rb
+++ db/migrate/20140721152826_create_locations.rb
@@ -1,9 +1,11 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-      t.string :location, null: false, unique: true
+      t.string :location, null: false
 
       t.timestamps null: false
     end
+
+    add_index :locations, [:location], unique: true
   end
 end
