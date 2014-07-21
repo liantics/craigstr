5953fa724355db50feb45ea5d0a4066399f2f22c
Liane Allen <liantics.antics@gmail.com>
2014-07-21 15:41:29 -0400

    Create ability to make a post
    - Add post form to index page - will need to add user login conditional in
    next step.
    - Add post model
    - Update post controller to allow posts to be created and displayed
    - Add database table for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
   def index
-  
+    @posts = Post.all
+    @post = Post.new 
+  end
+
+  def create
+    @posts = Post.all
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to :posts
+    else 
+      render :index
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+    )
   end
 end
