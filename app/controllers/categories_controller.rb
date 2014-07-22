78d137a188b48052e45430b49857aac0111ae5fb
Andrew Slawter <aslawter@gmail.com>
2014-07-22 11:55:02 -0400

    Redirect to posts after creating new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,7 +11,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to :categories
+      redirect_to :posts
     else
       render :index
     end
