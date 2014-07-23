fc1250cf1fed48390a9dd245bc990bd9be7661d3
Andrew Slawter <aslawter@gmail.com>
2014-07-23 11:24:00 -0400

    Fix spacing error in migration file

--- db/migrate/20140722142312_create_post_categories.rb
+++ db/migrate/20140722142312_create_post_categories.rb
@@ -3,7 +3,7 @@ class CreatePostCategories < ActiveRecord::Migration
     create_table :post_categories do |t|
       t.integer :post_id, null: false 
       t.integer :category_id, null: false
-      t.timestamps null:false
+      t.timestamps null: false
     end
     add_index :post_categories, [:post_id, :category_id], unique: true 
   end
