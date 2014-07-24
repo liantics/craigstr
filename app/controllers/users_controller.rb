c2efe76ae418d3a534e21ff670cf035a12a03090
ChristopherThorsen <Thorsen41@gmail.com>
2014-07-24 15:08:38 -0400

    make a side dashboard with easy links for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = current_user
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   resources :posts, only: [:index, :update, :destroy, :show, :edit] do
     resource :spam, only: [:create]
