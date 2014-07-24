ca1a503b8b6129689c2e266c32b9509f63cdbb54
Andrew Slawter <aslawter@gmail.com>
2014-07-24 14:56:19 -0400

    Limit categories to 5 per location on posts show
    *Allows alphabetical listing of 5 categories per location
    *Simplifies viewing
    *Provides consistency of category numbers per location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,11 @@
 class Location < ActiveRecord::Base
+  MAX_CATEGORIES_DISPLAYED = 5
   validates :name, presence: true, uniqueness: true
 
   has_many :categories
   has_many :posts
+
+  def featured_categories
+    categories.limit(MAX_CATEGORIES_DISPLAYED)
+  end 
 end
