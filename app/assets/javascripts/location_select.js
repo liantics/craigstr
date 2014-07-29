8fa9865e735cc95c19478525a674d249f73566c8
Liane Allen <liantics.antics@gmail.com>
2014-07-29 09:49:09 -0400

    Address Jsteiner and Gabebw comments

--- app/assets/javascripts/location_select.js
+++ app/assets/javascripts/location_select.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('#choose_location').on( 'click', function(){ 
+  $('#choose_location').on('click', function(){ 
     var choose_location = $("#choose_location");
     var location_id = $("#location_id").val()
     var new_path = window.location.pathname + "/" + location_id
