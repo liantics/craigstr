c97783368f10b1a252ee99df822e2b3f4939f25f
Liane Allen <liantics.antics@gmail.com>
2014-07-29 09:42:13 -0400

    Address Steiner's comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,7 @@ class PostsController < ApplicationController
   def show
     @user = current_user
     @post = Post.find(params[:id])
-    location_id = @post.location.id
-    @location = Location.find(location_id)
+    @location = @post.location 
   end
 
   def create
