464db88576f7e5fe6ef40535433ee919f4054d46
Liane Allen <liantics.antics@gmail.com>
2014-07-29 14:58:46 -0400

    Add neat formatting to photo and post boxes

--- db/migrate/20140726112501_add_image_url_to_posts.rb
+++ db/migrate/20140726112501_add_image_url_to_posts.rb
@@ -1,5 +1,5 @@
 class AddImageUrlToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :image_url, :string
+    add_column :posts, :image_url, :string, null: false, default: ""
   end
 end
